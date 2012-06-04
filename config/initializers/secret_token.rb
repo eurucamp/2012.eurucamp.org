d6bef5b1f80b4bfb89ae2ec89cfc0e3dd1b3afa6
Piotr Gega (pietia) <piotrgega@gmail.com>
2012-06-04 21:25:47 +0200

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eurucamp::Application.config.secret_token = '3d76d5fb47fa1306dfe72a4d5b78985a02ede23be6da6ebbaa0ce84b02793a59e88fa388d3b562d5a8ea6a72dcfaf406e1e983a2f5f4dc5ee877d73fd1da256c'
